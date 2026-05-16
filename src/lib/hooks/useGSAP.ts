794ffb040db3c56a58b30618e26e677a3a39dd60
khadka-sumit <sumitkhadka708@gmail.com>
2026-05-16 17:15:02 +0545

    Update useGSAP.ts

--- src/lib/hooks/useGSAP.ts
+++ src/lib/hooks/useGSAP.ts
@@ -4,6 +4,11 @@ import { useGSAP } from '@gsap/react';
 import { gsap, ScrollTrigger } from '@/lib/animation/gsap';
 import { useEffect } from 'react';
 
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(useGSAP);
+}
+
+
 // Custom hook to kill all scroll triggers on unmount if needed
 export const useScrollTrigger = () => {
   useEffect(() => {
